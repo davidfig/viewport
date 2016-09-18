57c2c94894e4ef0a88ab049dded2ea73caf33935
David Figatner <david@yopeyopey.com>
2016-09-18 22:28:34 +0800

    Removing unnecessary files

--- refresh.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-/usr/bin/osascript <<EOF
-tell application "Google Chrome"
-  set docUrl to URL of (active tab of window 1)
-  set URL of (active tab of window 1) to docUrl
-  activate
-end tell
-EOF
